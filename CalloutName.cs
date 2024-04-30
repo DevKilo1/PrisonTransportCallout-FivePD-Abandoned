0821bf9fea765e268d2e3c0a7319e8a8a74f1b50
Kilo <luknik2012@gmail.com>
2024-04-30 15:53:09 -0400

    .

--- CalloutName.cs
+++ CalloutName.cs
@@ -1,10 +1,34 @@
 ﻿using System.Drawing;
+using System.Threading.Tasks;
+using CitizenFX.Core;
 using FivePD.API;
+using Kilo.Commons.Utils;
 
 namespace CalloutTemplate
 {
     public class CalloutName : Callout
     {
+        public CalloutName()
+        {
+            ShortName = "CalloutName";
+            CalloutDescription = "";
+            StartDistance = 120f;
+            ResponseCode = 3;
+        }
+
+        public override Task OnAccept()
+        {
+            return base.OnAccept();
+        }
         
+        public override void OnStart(Ped closest)
+        {
+            base.OnStart(closest);
+        }
+
+        public override void OnCancelBefore()
+        {
+            base.OnCancelBefore();
+        }
     }
 }
