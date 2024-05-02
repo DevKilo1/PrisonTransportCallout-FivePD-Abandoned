d3a3a386a9447c83bb20fb9272d2ca0eca74292b
Kilo <luknik2012@gmail.com>
2024-05-01 21:42:46 -0400

    .

--- PrisonTransport.cs
+++ PrisonTransport.cs
@@ -1,25 +1,60 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Threading.Tasks;
 using CitizenFX.Core;
 using FivePD.API;
+using FivePD.API.Utils;
+using Kilo.Commons;
+using Kilo.Commons.Config;
 using Kilo.Commons.Utils;
+using Newtonsoft.Json.Linq;
 
 namespace Callouts
 {
+    [CalloutProperties("Prison Transport","DevKilo","1.0")]
     public class PrisonTransport : Callout
     {
         public PrisonTransport()
         {
-            Utils.Draw3DText(Vector3.Zero, "");
-            ShortName = "CalloutName";
-            CalloutDescription = "";
-            StartDistance = 120f;
-            ResponseCode = 3;
+            try
+            {
+                InitInfo(Game.PlayerPed.Position);
+                ShortName = "CalloutName";
+                CalloutDescription = "";
+                StartDistance = 120f;
+                ResponseCode = 3;
+            }
+            catch (Exception ex)
+            {
+                Utils.CalloutError(ex, this);
+            }
         }
 
-        public async override Task OnAccept()
+        private JObject defaultConfig = new JObject()
         {
             
+        };
+
+        private Vehicle bus;
+        private Ped driver;
+        private List<Ped> prisoners = new List<Ped>();
+        public async override Task OnAccept()
+        {
+            AcceptHandler();
+        }
+
+        private async Task AcceptHandler()
+        {
+            bus = await Utils.SpawnVehicleOneSync(VehicleHash.PBus, Location);
+            int numberOfPrisoners = RandomUtils.GetRandomNumber(1, bus.PassengerCapacity);
+            for (int i = 0; i < numberOfPrisoners; i++)
+            {
+                Ped p = await Utils.SpawnPedOneSync(Utils.GetRandomPed(), Location, true);
+                if (p == null) continue;
+                prisoners.Add(p);
+                await BaseScript.Delay(0);
+            }
         }
         
         public override void OnStart(Ped closest)
@@ -28,7 +63,17 @@ namespace Callouts
 
         public override void OnCancelBefore()
         {
-           
+            foreach (var entity in Utils.EntitiesInMemory.ToArray())
+            {
+                if (entity == null) continue;
+                if (entity.Model.IsPed)
+                {
+                    var ped = (Ped)entity;
+                    if (ped.IsCuffed) continue;
+                    if (ped.IsDead) continue;
+                }
+                entity.MarkAsNoLongerNeeded();
+            }
         }
     }
 }
